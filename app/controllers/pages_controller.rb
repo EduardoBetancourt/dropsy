74c2e2a7510409fca64ba4dc182912847cd36c09
Eduardo Betancourt <ebhdz@hotmail.com>
2021-03-07 22:53:02 -0600

    Almost there

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,14 @@
+require 'csv'
+
 class PagesController < ApplicationController
   def index
+    @workouts = Workout.all
+    @wod = Workout.all[0]
+  end
+
+  def upload_csv
+    CSV.foreach('db/csv/workouts.csv', headers: true) do |row|
+      Workout.create(date: row[0], description: row[1])
+    end
   end
 end
